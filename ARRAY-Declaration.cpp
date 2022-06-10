76976b89c584ed06cf077fb344e5502aa6ef81f6
Rohit Thakur <105488212+its-me-rohit@users.noreply.github.com>
2022-06-10 22:53:07 +0530

    Create ARRAY-Declaration.cpp

---
+++ ARRAY-Declaration.cpp
@@ -0,0 +1,16 @@
+Array declaration
+
+Which of the following correctly declares an array of size 10 ?
+
+
+Options
+
+This problem has only one correct answer : 
+
+(1)int array[10]; 
+(2)int array;
+(3)array{10};
+(4)array array[10];
+
+
+Correct Answer = (1) int array[10]; 
